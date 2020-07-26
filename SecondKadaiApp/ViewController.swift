72d03171a09d09e5a9fa8b3042f00771529d3ed6
tomomi.koyama <smalllamp.t.k@gmail.com>
2020-07-26 18:06:45 +0900

    Initial Commit

---
+++ SecondKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecondKadaiApp
+//
+//  Created by 小山燈実 on 2020/07/26.
+//  Copyright © 2020 tomomi.koyama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
